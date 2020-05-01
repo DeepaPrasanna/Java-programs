a699b01e267df282469ed44c80dd3d0c0674ffe3
DeepaPrasanna <anneraj73@gmail.com>
2020-05-01 21:50:58 +0530

    mirror clock

---
+++ mirrorClock.java
@@ -0,0 +1,45 @@
+import java.util.*; public class MirrorClock
+{
+public static void main(String args[])
+{
+int mm,hh;
+Mirror m = new Mirror();
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the HH:");
+hh = scn.nextInt();
+System.out.println("Enter the minute:");
+mm = scn.nextInt();
+m.mirror1(hh,mm);
+}
+}
+class Mirror
+{
+int xh,xm;
+void mirror1(int hh, int mm)
+{
+xm = 60 - mm;
+if(hh<12)
+{
+if(mm==0)
+{
+xh = 12-hh;
+}
+else
+{
+xh=11-hh;
+}
+}
+else
+{
+if(mm == 0)
+{
+xh=12;
+}
+else
+{
+xh=11;
+}
+}
+System.out.println("mirror of"+hh+"::"+mm+"="+xh+":"+xm);
+}
+}
